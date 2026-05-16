bf8b221313447885737580c8c3d2a4e81e771df1
Yobeco <ybecognee@yahoo.fr>
2026-05-16 12:44:12 -0600

    Improvment of the flowchart.

--- code/parser.py
+++ code/parser.py
@@ -6,6 +6,12 @@ import re
 
 # --- Fonction pour récupérer le contenu d'un code QR Wifi ---
 def wifi_parser(raw_str: str):
+    """
+    Analyse le contenu du code QR de configuration wifi (non crypté)
+    :param raw_str: Chaine de caractères utf-8
+    :return: tuple de str au format ('<type_de_sécurité>', '<SSID>', '<mot_de_passe>', '<bool>')
+    """
+
     # 1. Valider et nettoyer le préfixe (redondant)
     if not re.match(r"^WIFI:", raw_str, re.IGNORECASE):
         raise ValueError("Erreur de syntax QR Wifi")
@@ -48,33 +54,40 @@ def wifi_parser(raw_str: str):
 
 # --- Méthode pour analyser tous les contenus de QR possibles
 def qr_parser(texte):
+
+    # 1er élément du tuple permettant d'orienter le match/case vers l'analyse adéquate
+    # Orienter vers l'analyseur d'ID opaques (Drives en ligne)
+    id_IDopaque = "id_op"       # Pour reconstruire l'URL complète
+    # Orienter vers l'analyseur de fichiers inclus dans l'app
+    id_assets = "assets"        # Orienter vers l'
+
     match texte:
 
-        # 1. --> Cas des QR Wifi (Script encore mal compris)
+        # 0. --> Cas des QR Wifi (Script encore mal compris)
+        # --> ⚠️ À séparer pour un trainement à part !
         case str(s) if s.upper().startswith("WIFI:"):
             # print("C'est un Wifi")
             # print(wifi_parser(str(s)))
             return wifi_parser(str(s))
 
-        # 2. --> Cas des Liens vers Google Drive (Implémenter Drive "La suite")
-        # On cherche un motif <gd> au début, et on capture l'ID du fichier
-        case str(s) if (m := re.match(r"^/(gd)/(.*)", s)):
-            drive_name = m.group(1)     # Contient 'gd', 'md' ou '??' (Selon le type de Cloud)
-            file_id = m.group(2)        # Contient le reste du texte
 
-            return (drive_name, file_id)   # Pas besoin de renvoyer la valeur
-            # --> Lancer la méthode directement avec ces paramètres
+        # 1. --> Cas des Liens vers des Drive sur internet /xx/ : identifiant opaque (permalink)
+        case str(s) if (m := re.match(r"^/([^/]+)/(.*)", s)):
+            drive_name = m.group(1)  # Tout ce qui est entre le 1er et le 2e "/"
+            file_id = m.group(2)  # Le reste après le 2e "/"
 
-        # 3. Cas des fichiers .mp3 locaux
+            return id_IDopaque, drive_name, file_id
+
+
+        # 2. Cas des fichiers .mp3 inclus dans l'application
         case str(s) if (m := re.match(r"^_([a-z]{2})_(.*)", s)):
-            folder_url = m.group(1)  #
-            file_name = m.group(2)  #
+            language = m.group(1)  #
+            word = m.group(2)  #
 
-            return (f"assets/{folder_url}", f"_{folder_url}_{file_name}.mp3")
+            return id_assets, language, word
 
 
-
-        # 4. --> Cas des QR avec contenu à vocaliser
+        # 3. --> Cas des QR avec contenu à vocaliser
         # Groupe 1 : langue (optionnel) --> 2 ou 3 lettres min / maj
         # Groupe 2 : contenu (obligatoire, mais peut être vide)
         # Groupe 3 : suffixe après l'étoile (optionnel)
@@ -99,12 +112,13 @@ tests = [
     r"WIFI:T:<type_de_sécurité>;S:<SSID>;P:<mot_de_passe>;H:<caché_ou_non>;",
     r"WIFI:T:WEP;S:CLARO_5697Rg;P:Ne1ed1TaPers0nne;H:true;",
     "/gd/5sq564fze546cx8er8q",
+    "/ls/ed49c810-21bd-4b92-bce5-3ca4d3b83a79",
     "<fr>Bonjour#e",
     "<es>Hola !#d",
     "<en>Hi!",
     "Coucou#e",
     "Ça va ?#d",
-    "Ça fonctionne bien",
+    "Ça fonctionne sans balise",
     "_bm_ou",
     "_es_GATEAU",
     "<frC>Hola !#d",
